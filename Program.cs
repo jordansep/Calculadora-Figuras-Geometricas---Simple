4a928a2b3d0175c50935abbdc2bd7468b898f1e6
jordansep <jordanmonier4@gmail.com>
2025-07-15 22:19:03 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Figuras_Geometricas___Simple
+{
+    internal class Program
+    {
+        /* 2. Calculadora de Figuras Geométricas
+                Diseña un programa que calcule el área y el perímetro de diferentes figuras geométricas.
+                
+                Clase Abstracta/Interfaz: Crea una clase abstracta Figura o una interfaz IFigura con métodos para calcular Area() y Perimetro().
+                
+                Clases Derivadas: Implementa clases como Circulo, Rectangulo y Triangulo que hereden de Figura (o implementen IFigura).
+                
+                Polimorfismo: Usa polimorfismo para manejar las diferentes figuras en una colección y calcular sus propiedades.
+                
+                Entrada de Usuario: Pide al usuario que elija una figura e ingrese los datos necesarios (radio, lados, etc.). */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
