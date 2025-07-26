57317971beb0e61e6d7e799b4e55d89de2e64f49
jordansep <jordanmonier4@gmail.com>
2025-07-26 17:44:09 -0300

    superClase e Interfaz

---
+++ Modules/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class FiguraGeometrica
+{
+    private float ancho;
+    private float largo;
+    private float area;
+    private float lados;
+
+
+    public FiguraGeometrica(float ancho, float largo, float area, float lados) { 
+        this.ancho = ancho;
+        this.largo = largo;
+        this.area = area;
+        this.lados = lados;
+    }
+}
+
+interface IFigurasBidimensionales
+{
+    void CalcularArea();
+
+}
